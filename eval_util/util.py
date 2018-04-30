4f005f415cbe1dbe3ec58965593c1c1d2c8c82dd
wakananai <wakananai@gmail.com>
2018-04-30 21:08:59 +0800

    modify the f1_score para

--- eval_util/util.py
+++ eval_util/util.py
@@ -4,4 +4,4 @@ def evalMatrix(ground,pred,mode):
     if mode == 'mse':
         return mean_squared_error(ground, pred)
     elif mode == 'f1_score':
-        return f1_score(ground,pred)
+        return f1_score(ground,pred,average=None)
