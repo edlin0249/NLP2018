4d378285e040d147bbb351b72bfb4ef638734f6d
wakananai <wakananai@gmail.com>
2018-05-04 16:44:43 +0800

    rm eval_util folder

--- eval_util/util.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import mean_squared_error,f1_score
-
-def evalMatrix(ground,pred,mode):
-    if mode == 'mse':
-        return mean_squared_error(ground, pred)
-    elif mode == 'f1_score':
-        return f1_score(ground,pred,average=None)
